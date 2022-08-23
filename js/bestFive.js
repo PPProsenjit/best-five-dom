a0c736a4cb3b2f4a1bd7b37f640c386e04f1d3f5
PPProsenjite <prosenjitpappu03455@gmial.com>
2022-08-23 06:02:18 +0600

    fooler and selected five add

--- js/bestFive.js
+++ js/bestFive.js
@@ -1,36 +1,50 @@
 
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
-
+document.getElementById('btn-messi').addEventListener('click', function () {
+    checkPlayerListById('selective-player');
+    const getTitleName = getTextPlayerTitleById('player-title-messi');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-messi').disabled = true;
+    document.getElementById("btn-messi").style.backgroundColor = "lightblue";
 })
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
 
+document.getElementById('btn-neymar').addEventListener('click', function () {
+    checkPlayerListById('selective-player');
+    const getTitleName = getTextPlayerTitleById('player-title-neymar');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-neymar').disabled = true;
+    document.getElementById("btn-neymar").style.backgroundColor = "lightblue";
 })
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
 
+document.getElementById('btn-kylian').addEventListener('click', function () {
+    checkPlayerListById('selective-player');
+
+    const getTitleName = getTextPlayerTitleById('player-title-kylian');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-kylian').disabled = true;
+    document.getElementById("btn-kylian").style.backgroundColor = "lightblue";
 })
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
 
+document.getElementById('btn-vitor').addEventListener('click', function () {
+     checkPlayerListById('selective-player');
+    const getTitleName = getTextPlayerTitleById('player-title-vitor');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-vitor').disabled = true;
+    document.getElementById("btn-vitor").style.backgroundColor = "lightblue";
 })
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
 
+document.getElementById('btn-renato').addEventListener('click', function () {
+    checkPlayerListById('selective-player');
+
+    const getTitleName = getTextPlayerTitleById('player-title-renato');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-renato').disabled = true;
+    document.getElementById("btn-renato").style.backgroundColor = "lightblue";
 })
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
 
-})
-document.getElementById('btn-messi').addEventListener('click', function () {   
-   getInputPlayerName("player-messi");
-    setPlayerNameInDashboard('display-player');
-
+document.getElementById('btn-sergio').addEventListener('click', function () {
+    checkPlayerListById('selective-player');
+    const getTitleName = getTextPlayerTitleById('player-title-sergio');
+    setPlayerNameInListById('selective-player', getTitleName);
+    document.getElementById('btn-sergio').disabled = true;
+    document.getElementById("btn-sergio").style.backgroundColor = "lightblue";
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,51 @@
-let playerNameString = '';
-function getInputPlayerName(playerId) {
-    const playerName = document.getElementById(playerId);
-    playerNameString = playerName.innerText;
-    return playerNameString;
+function checkPlayerListById(elementId) {
+    let playerList = document.getElementById(elementId);
+    let list = playerList.children;
+    if (list.length > 4 ) {
+        alert("player has been selected!");
+        return exit;
+    }
+    return list.length;
 }
-function setPlayerNameInDashboard(boardId) {
-    const playerDashboard = document.getElementById(boardId);
-    playerDashboard.innerText = playerNameString;
-    
+
+function setPlayerNameInListById(elementId, newValue) {
+    const getPlayer = document.getElementById(elementId);
+    const li = document.createElement('li');
+    li.innerText = newValue;
+    getPlayer.appendChild(li);
+
+    return getPlayer;
 }
+
+function getTextPlayerTitleById(elementId) {
+    const playerTitleText = document.getElementById(elementId);
+    const playerTitleTextString = playerTitleText.innerText;
+
+    return playerTitleTextString;
+}
+
+
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldString);
+
+    return inputFieldValue;
+
+}
+
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textValue = parseFloat(textElementValueString);
+
+    return textValue;
+
+}
+
+function setTextElementValueById(elementId, newValue) {
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+
+}
+
